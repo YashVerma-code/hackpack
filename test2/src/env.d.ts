82239448d910b57c877f43a03536632a96fe9878
vscodes2003 <bt23cse009@iiitn.ac.in>
2025-11-17 19:24:51 +0530

    clerk vue fix

--- test2/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
-// Allow alias imports for @/
-declare module '@/*'
